816c486dd6c0b45402fe1d919e48ed74d81dfc68
Zanis Ali <zanisali999@gmail.com>
2018-12-22 18:44:42 +0100

    Initial commit

---
+++ app/src/main/java/startup/softflix/com/firebasedemoproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package startup.softflix.com.firebasedemoproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
